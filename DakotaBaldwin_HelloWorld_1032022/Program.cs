5af43070ae5d574d1b44869b865d95d51d4e1b02
Dakota Baldwin <dakota.baldwin@onshoreoutsourcing.com>
2022-10-04 08:06:58 -0500

    Will this finally work for me?

---
+++ DakotaBaldwin_HelloWorld_1032022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _DakotaBaldwinHelloWorld_1032022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Collect name from user
+            Console.WriteLine("Please enter your name.");
+            string creatorName = Console.ReadLine();
+
+            // Inject usrName variable into Console string
+            Console.WriteLine("Hi World, " + creatorName + " programmed me! Thank you " + creatorName + "!");
+            Console.ReadKey();
+        }
+    }
+}
